99cdbf70e1f16486b91dbfed64bd88cd5cb5ac4f
Mykhailo <mmmalets10@gmail.com>
2026-02-16 13:23:03 +0200

    Refactor DXGI/MediaEngine init and improve error handling
    Refactored DirectX, DXGI, and Media Foundation initialization in MainWindow for better resource management and error handling. Replaced deviceManager with dxgiManager, standardized use of check_hresult, and improved swap chain and render target setup. Moved DispatcherTimer setup to a dedicated method. Removed redundant code and updated member variables for clarity and maintainability.

--- MainWindow.xaml.cpp
+++ MainWindow.xaml.cpp
@@ -19,7 +19,7 @@ namespace winrt::MediaPlayer::implementation
     }
 
     void MainWindow::InitializeDirectX() {
-        winrt::check_hresult(D3D11CreateDevice(
+        check_hresult(D3D11CreateDevice(
             nullptr,
             D3D_DRIVER_TYPE_HARDWARE,
             nullptr,
@@ -32,10 +32,12 @@ namespace winrt::MediaPlayer::implementation
             d3dDeviceContext.put())
         );
 
-        dxgiDevice = d3dDevice.as<IDXGIDevice>();
+        check_hresult(MFCreateDXGIDeviceManager(&resetToken, dxgiManager.put()));
+        check_hresult(dxgiManager->ResetDevice(d3dDevice.get(), resetToken));
     }
 
     void MainWindow::InitializeSwapChain() {
+        auto panelNative = this->SwapChainCanvas().as<ISwapChainPanelNative>();
 
         uint32_t width = static_cast<uint32_t>(this->SwapChainCanvas().ActualWidth());
         uint32_t height = static_cast<uint32_t>(this->SwapChainCanvas().ActualHeight());
@@ -50,52 +52,64 @@ namespace winrt::MediaPlayer::implementation
         swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
         swapChainDesc.BufferCount = 2;
         swapChainDesc.SampleDesc.Count = 1;
-        swapChainDesc.SampleDesc.Quality = 0;
 		swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
-		swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
 		swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_PREMULTIPLIED;
 		swapChainDesc.Flags = 0;
-		swapChainDesc.Stereo = FALSE;
+		swapChainDesc.Stereo = false;   
 
+        auto dxgiDevice = d3dDevice.as<IDXGIDevice>();
 		com_ptr<IDXGIAdapter> dxgiAdapter;
-		dxgiDevice.get()->GetAdapter(dxgiAdapter.put());
-		com_ptr<IDXGIFactory2> dxgiFactory2;
-		winrt::check_hresult(dxgiAdapter.get()->GetParent(__uuidof(IDXGIFactory2), dxgiFactory2.put_void()));
-		winrt::check_hresult(dxgiFactory2->CreateSwapChainForComposition(dxgiDevice.get(), &swapChainDesc, nullptr, swapChain.put()));
+        check_hresult(dxgiDevice->GetAdapter(dxgiAdapter.put()));
+		
+        com_ptr<IDXGIFactory2> dxgiFactory2;
+        check_hresult(
+            dxgiAdapter->GetParent(IID_PPV_ARGS(dxgiFactory2.put()))
+        );
 
-        auto panelNative = this->SwapChainCanvas().as<ISwapChainPanelNative>();
-        winrt::check_hresult(panelNative->SetSwapChain(swapChain.get()));
+		check_hresult(
+            dxgiFactory2->CreateSwapChainForComposition(
+                dxgiDevice.get(), &swapChainDesc, nullptr, swapChain.put()
+            )
+        );
 
-        swapChain.get()->GetBuffer(0, __uuidof(backBuffer), backBuffer.put_void());
-        d3dDevice->CreateRenderTargetView(backBuffer.get(), nullptr, renderTargetView.put());
+        check_hresult(panelNative->SetSwapChain(swapChain.get()));
+        check_hresult(swapChain.get()->GetBuffer(0, IID_PPV_ARGS(backBuffer.put())));
+        check_hresult(
+            d3dDevice->CreateRenderTargetView(
+                backBuffer.get(), nullptr, renderTargetView.put()
+            )
+        );
 	}
 
     void MainWindow::InitializeMediaEngine() {
-        winrt::com_ptr<IMFMediaEngineClassFactory> factory;
+        com_ptr<IMFMediaEngineClassFactory> factory;
         
-        winrt::check_hresult(CoCreateInstance(
-            IID_IMFMediaEngineClassFactory,
+        check_hresult(CoCreateInstance(
+            CLSID_MFMediaEngineClassFactory,
             nullptr,
             CLSCTX_INPROC_SERVER,
             IID_PPV_ARGS(factory.put()))
         );
 
         com_ptr<IMFAttributes> attr;
-        winrt::check_hresult(MFCreateAttributes(attr.put(), 1));
+        check_hresult(MFCreateAttributes(attr.put(), 1));
         
-        auto notify = winrt::make<MediaEngineNotify>();
-        winrt::check_hresult(attr->SetUnknown(MF_MEDIA_ENGINE_CALLBACK, notify.get()));
-        
-        winrt::check_hresult(attr->SetUINT32(MF_MEDIA_ENGINE_VIDEO_OUTPUT_FORMAT, 21)); // D3DFMT_A8R8G8B8
-        
-        MFCreateDXGIDeviceManager(&resetToken, deviceManager.put());
-        deviceManager->ResetDevice(dxgiDevice.get(), resetToken);
-        winrt::check_hresult(attr->SetUnknown(MF_MEDIA_ENGINE_DXGI_MANAGER, deviceManager.get()));
+        auto notify = make<MediaEngineNotify>();
+        check_hresult(attr->SetUnknown(MF_MEDIA_ENGINE_CALLBACK, notify.get()));
+        check_hresult(attr->SetUINT32(MF_MEDIA_ENGINE_VIDEO_OUTPUT_FORMAT, DXGI_FORMAT_B8G8R8A8_UNORM));
+        check_hresult(attr->SetUnknown(MF_MEDIA_ENGINE_DXGI_MANAGER, dxgiManager.get()));
 
-        winrt::check_hresult(factory.get()->CreateInstance(
+        check_hresult(factory.get()->CreateInstance(
             0,
             attr.get(),
             me.put())
         );
+    }
+
+    void MainWindow::InitializeTimer() {
+        timer = DispatcherTimer();
+        timer.Interval(std::chrono::milliseconds(16));
+        timer.Tick({ this, &MainWindow::OnTimerTick });
+        timer.Start();
     }
 }
--- MainWindow.xaml.h
+++ MainWindow.xaml.h
@@ -7,27 +7,22 @@
 namespace winrt::MediaPlayer::implementation {
     struct MainWindow : MainWindowT<MainWindow> {
         MainWindow() {
-            //TODO: refactor
             InitializeComponent();
 
             this->ExtendsContentIntoTitleBar(true);
             this->SetTitleBar(AppTitleBar());
 
-            timer = winrt::Microsoft::UI::Xaml::DispatcherTimer();
-			timer.Interval(std::chrono::milliseconds(100 / 60));
-            timer.Tick({this, &MainWindow::OnTimerTick });
-			timer.Start();
-
+            MFStartup(MF_VERSION);
 			InitializeDirectX();
 			InitializeSwapChain();
+            InitializeMediaEngine();
+            InitializeTimer();
         }
 
         void MyButton_Click(
             winrt::Windows::Foundation::IInspectable const& sender,
             winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e) {
-            MFStartup(MF_VERSION);
-
-            InitializeMediaEngine();
+            
         }
 
     private:
@@ -39,18 +34,18 @@ namespace winrt::MediaPlayer::implementation {
 
 		com_ptr<ID3D11Device> d3dDevice;
         com_ptr<ID3D11DeviceContext> d3dDeviceContext;
-		com_ptr<IDXGIDevice> dxgiDevice;
         com_ptr<IDXGISwapChain1> swapChain;
 		com_ptr<ID3D11Texture2D> backBuffer;
 		com_ptr<ID3D11RenderTargetView> renderTargetView;
         com_ptr<IMFMediaEngine> me;
-        com_ptr<IMFDXGIDeviceManager> deviceManager;
+        com_ptr<IMFDXGIDeviceManager> dxgiManager;
 
         UINT resetToken;
 
         void InitializeDirectX();
         void InitializeSwapChain();
         void InitializeMediaEngine();
+        void InitializeTimer();
     };
 }
 
